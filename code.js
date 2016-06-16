4c049b95ff623d5e78450c3ecaddd792f05a742c
Nerdherdx <nerdherdx212@gmail.com>
2016-06-15 21:37:19 -0400

    All files.

---
+++ code.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("div#page").css("background-color","black");
+    $("div#watch-header").css("background-color","black");
+    $("div#watch-discussion").css("background-color","black");
+    $("div#watch7-sidebar-contents").css("background-color","black");
+});
